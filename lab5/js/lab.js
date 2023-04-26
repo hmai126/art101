7fbbf050edaa2d1d1159a3a6ae96fb9e2bb24b3b
hmai126 <129999899+hmai126@users.noreply.github.com>
2023-04-25 17:43:15 -0700

    Lab 5 done

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab.js - Lab 5 JavaScript file to hold and print data for my method of transportation.
+// Author: Hilton Mai
+// Date: April 25, 2023
 
 // Constants
 
@@ -15,6 +15,22 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+
+  // define and assign variables
+  var make = "Honda";
+  var model = "Accord";
+  var color = "White";
+  var year = 2021;
+
+  // calculate
+  var age = 2023 - year;
+  
+  // output
+  document.writeln("Make: " + make + "<br>");
+  document.writeln("Model: " + model + "<br>");
+  document.writeln("Color: " + color + "<br>");
+  document.writeln("Year: " + year + "<br>");
+  document.writeln("Age: " + age + "<br>");
 }
 
 // let's get this party started
