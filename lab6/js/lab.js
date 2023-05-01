cedc694cf732f4c529853e7bb4215da54f1238b4
hmai126 <129999899+hmai126@users.noreply.github.com>
2023-05-01 00:34:41 -0700

    updated stuff for lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,20 +1,37 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab.js - This JavaScript file will print data from an array and object onto a webpage.
+// Author: Hilton Mai
+// Date: April 28, 2023
 
 // Constants
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
+// this is an example function and this comment tells what it does and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
   // return results;
 }
 
 function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+  // Define variables
+  var myTransport = ["E-bike", "Honda Accord", "Metro Bus"];
+
+  // Create an object for my main ride
+  var myMainRide = {
+    make: "Honda",
+    model: "Accord",
+    color: "white",
+    year: 2021,
+    age: function() {
+        return 2023 - this.year;
+    }
+  }
+
+  // Output
+  document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+
+  // Write an object to the document
+  document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
 }
 
 // let's get this party started
