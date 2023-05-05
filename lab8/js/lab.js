e5f0f5e0e906838e9c5d0888cdf25731691e500b
hmai126 <129999899+hmai126@users.noreply.github.com>
2023-05-05 09:32:05 -0700

    updated lab.js for lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -14,6 +14,7 @@ function main() {
 
     var numArray = [1, 2, 6, 9, 7, 10];
     console.log("My array: ", numArray);
+    // print original array to HTML file
     var printInfo  = document.getElementById("arrayInfo1");
     printInfo.innerHTML = "The Original Array: ";
     var outputEl = document.getElementById("scriptOutput1");
@@ -22,17 +23,20 @@ function main() {
     var result = numArray.map(addNum);
     // should return [6, 7, 11, 14, 12, 15]
     console.log("Test of array elements + 5: ", result);
+    // print array with elements + 5 to HTML file
     var printInfo2 = document.getElementById("arrayInfo2");
     printInfo2.innerHTML = "The Array With Elements + 5: ";
     var outputEl2 = document.getElementById("scriptOutput2");
     outputEl2.innerHTML = JSON.stringify(result);
 
+    // callback function to square a number
     var result = numArray.map(function(x) {
         return x ** 2;
     })
 
     // should return [1, 4, 36, 81, 49, 100]
     console.log("This array squared is: ", result)
+    // print array with elements squared to HTML file
     var printInfo3 = document.getElementById("arrayInfo3");
     printInfo3.innerHTML = "The Array Elements Squared: ";
     var outputEl3 = document.getElementById("scriptOutput3");
