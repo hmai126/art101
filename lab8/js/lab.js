3c051a7490fdce6214715069f9b95191d2b6c3ca
hmai126 <129999899+hmai126@users.noreply.github.com>
2023-05-05 10:07:12 -0700

    edited the lab 8 files even more

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,4 +1,6 @@
-// lab.js - This JavaScript file 
+// lab.js - This JavaScript file will utilize anonymous
+//          and callback functions to edit elements in
+//          an array.
 // Author: Hilton Mai
 // Date: May 4, 2023
 
@@ -14,6 +16,7 @@ function main() {
 
     var numArray = [1, 2, 6, 9, 7, 10];
     console.log("My array: ", numArray);
+
     // print original array to HTML file
     var printInfo  = document.getElementById("arrayInfo1");
     printInfo.innerHTML = "The Original Array: ";
@@ -23,6 +26,7 @@ function main() {
     var result = numArray.map(addNum);
     // should return [6, 7, 11, 14, 12, 15]
     console.log("Test of array elements + 5: ", result);
+
     // print array with elements + 5 to HTML file
     var printInfo2 = document.getElementById("arrayInfo2");
     printInfo2.innerHTML = "The Array With Elements + 5: ";
@@ -36,6 +40,7 @@ function main() {
 
     // should return [1, 4, 36, 81, 49, 100]
     console.log("This array squared is: ", result)
+
     // print array with elements squared to HTML file
     var printInfo3 = document.getElementById("arrayInfo3");
     printInfo3.innerHTML = "The Array Elements Squared: ";
