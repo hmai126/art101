f475a325f088789b8aeabd27b7c8382190960712
hmai126 <129999899+hmai126@users.noreply.github.com>
2023-05-17 00:52:25 -0700

    updated the lab 11 files again

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -3,11 +3,19 @@
 // Author: Hilton Mai
 // Date: May 16, 2023
 
+
+// Find the section you want to add the button to
+// Create the button
+// Give it a text label
+// Add it to the section
 $("#challenge").append("<button id='my-button'>Press me!</button>")
 $("#problems").append("<button id='my-button2'>Press me!</button>")
 $("#reflection").append("<button id='my-button3'>Press me!</button>")
 $("#results").append("<button id='my-button4'>Press me!</button>")
 
+// Add a click event to each button
+// Find the challenge, problems, reflections, or results section the button is in.
+// Toggle a class special within the <div> of the section
 $("#my-button").click(function() {
     $("#challenge").toggleClass("special");
 });
