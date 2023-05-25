237bc0b0fa55559f745f204d29a54bc548798bca
hmai126 <129999899+hmai126@users.noreply.github.com>
2023-05-24 18:12:51 -0700

    updated lab 13 files

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,4 +1,78 @@
-// lab.js - This JavaScript file contains a sorting
-//          function that depends on conditionals.
+// lab.js - This JavaScript file contains a function
+//          that uses a for loop.
 // Author: Hilton Mai
 // Date: May 23, 2023
+
+var maxFactors = 4;
+
+var outputEl = $("#output");
+
+// function taken from Wes's JavaScript file, converted from pure JS to jQuery
+function fizzBuzzBoom(maxNums, factorObj) {
+    // iterate over all numbers
+    for (var i = 0; i < maxNums; i++) {
+        // reset output string
+        var outputStr = "";
+        // iterate over the factors we got from the HTML
+        for (var factor in factorObj) {
+            // check to see if this num is a multiple of factor
+            if (i % factor == 0) {
+                // if yes, then add the text to output string
+                outputStr += factorObj[factor];
+            }
+        }
+        if (outputStr) {
+            outputStr = " - " + outputStr + "!";
+        }
+        outputToPage(i.toString() + outputStr);
+    }
+}
+
+// function taken from Wes's JavaScript file, converted from pure JS to jQuery
+function getFactorObj() {
+    var factorObj = {};
+    for (var factor = 0; factor < maxFactors; factor++) {
+        numId = "num" + factor;
+        textId = "text" + factor;
+        numValue = $("#" + numId).val();
+        textValue = $("#" + textId).val();
+        console.log(factor + ") num:", numValue, "text:", textValue)
+        // if either value is blank, don't use it
+        if (numValue && textValue) {
+            factorObj[numValue] = textValue;
+        }
+    }
+    return factorObj;
+}
+
+// function taken from Wes's JavaScript file, converted from pure JS to jQuery
+function outputToPage(str) {
+    var newEl = $("<p></p>");
+    newEl.html(str);
+    outputEl.append(newEl);
+}
+
+// function taken from Wes's JavaScript file, converted from pure JS to jQuery
+function reportError(str) {
+    outputEl.html("<div class='error'>" + str + "</div>");
+}
+
+// function taken from Wes's JavaScript file, converted from pure JS to jQuery
+$("#submit").click(function() {
+    var max = $("#max").val();
+    console.log("max:", max);
+    if (! max) {
+        reportError("You must provide a maximum.");
+        return;
+    }
+    var factorObj = getFactorObj();
+    console.log("factorObj: ", factorObj);
+    if (Object.keys(factorObj).length === 0) {
+        reportError("You must provide at least one factor and text.");
+        return;
+    }
+    // clear error if there is one
+    outputEl.html("");
+    fizzBuzzBoom(max, factorObj);
+    outputEl.addClass("cols");
+})
